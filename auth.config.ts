26625ce03e796bce498d3b4f0204aea1576893ad
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-01-30 21:24:43 +0200

    auth error fixed

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ import Google from "next-auth/providers/google";
 import { User } from "@prisma/client";
 
 export default {
+  trustHost:true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
